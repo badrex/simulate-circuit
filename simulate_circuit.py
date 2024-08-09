db3ea55df4e88034f0409472bf1e75e4f8b76244
badrex <badr.nlp@gmail.com>
2024-08-09 17:32:28 +0200

    fix a bug and add voltmeter

--- simulate_circuit.py
+++ simulate_circuit.py
@@ -3,11 +3,48 @@
 import asyncio
 
 
+class Voltmeter:
+    """
+    A class to represent a voltmeter device to read voltage 
+    """
+    def __init__(self):
+        self.readings = []
+
+    def read(self, time_step, circuit):
+        """ read voltage voltage on RL in the circuit """ 
+
+        # compute voltage Vout across RL
+        try: 
+            R = (circuit.R2 * circuit.RL) / (circuit.R2 + circuit.RL)
+        except ZeroDivisionError: # if R1 + R2 = 0
+            R = float('inf')
+
+        # calculate total current in the circuit
+        I_L = circuit.Vs / (circuit.R1 + R)
+        
+        V_L = I_L * R
+
+        self.readings.append(
+            {
+                "time": time_step,
+                "R1": circuit.R1,
+                "R2": circuit.R2,
+                "R": R,
+                "voltage": V_L
+            }
+        ) 
+
+    def last_reading(self):
+    
+        """ return the last reading """
+        return self.readings[-1]
+
+
 class Circuit:
     """
     A class to simulate an electric circuit with in real-time
     """
-    def __init__(self, init_R1, init_R2, RL=30000, Vin=10):
+    def __init__(self, init_R1, init_R2, RL=30000, Vs=10):
         self.init_R1 = init_R1
         self.init_R2 = init_R2
 
@@ -15,34 +52,45 @@ class Circuit:
         self.R2 = self.init_R2
 
         self.RL = RL
-        self.Vin = Vin
+        self.Vs = Vs
 
         self.time_step_size = 100 # in msec
+
+        # instantiate a voltmeter
+        self.voltmeter = Voltmeter()
+
 
     def compute_total_R(self):
         """Return total resistance R of the circuit in Ohms"""
         try: 
-            R_total = self.R1 + ((1/self.R2) + (1/self.RL))
-        except ZeroDivisionError: # if R2 = 0
+            R_total = self.R1 + (self.R2 * self.RL) / (self.R2 + self.RL)
+        except ZeroDivisionError: # if R1 + R2 = 0
             R_total = float('inf')
 
         return R_total
 
-    async def update_status(self, time_step):
+
+    async def update_state(self, time_step):
         """ update circuit parameters given current time step in msec"""
         self.R1 = self.init_R1 + 10*time_step
         self.R2 = self.init_R2 - 10*time_step
 
         R_total = self.compute_total_R()
 
-        # Print current status
-        ohm = "\u03A9"
-        print(
-            f"t: {time_step:>6} msec "
-            f"R1: {self.R1/1000:>6.2f} k{ohm} "
-            f"R2: {self.R2/1000:>6.2f} k{ohm} "
-            f"Total R: {R_total/1000:>6.2f} k{ohm}"
-        )
+        # read voltage across RL
+        self.voltmeter.read(time_step, self)
+
+        print(self.voltmeter.last_reading())
+    
+
+        # Print current status - only for debugging
+        # ohm = "\u03A9"
+        # print(
+        #     f"t: {time_step:>6} msec "
+        #     f"R1: {self.R1/1000:>6.2f} k{ohm} "
+        #     f"R2: {self.R2/1000:>6.2f} k{ohm} "
+        #     f"Total R: {R_total/1000:>6.2f} k{ohm}"
+        # )
 
         await asyncio.sleep(self.time_step_size/1000)
 
@@ -52,24 +100,35 @@ class Circuit:
         start the simulation for 10 seconds with 100 msec timesteps
         """
         
-        # loop through the simulation time for steps of 1 msec
+        # update circuit state every 100 msec
         for t_step in range(0, 10100, self.time_step_size):
 
-            await self.update_status(t_step)
+            await self.update_state(t_step)
 
-    def restart(self):
-        """
-        Restart the simulation
-        """
+
+    async def restart(self):
+        """ restart the simulation """
+
+        # set R1 and R2 to initial values 
         self.R1 = self.init_R1
         self.R2 = self.init_R2
-        self.start()
 
-  
+        await self.start()
+
+
+async def main():
+    # Instantiate the Circuit class
+    circuit = Circuit(0, 100000, RL=30000, Vs=10)
+
+    # Start the simulation
+    await circuit.start()
+
+    #print(circuit.voltmeter.readings)
+
+
+    # Restart the simulation
+    #await circuit.restart()
 
 if __name__ == "__main__":
-    # instantiate the Circuit class
-    circuit = Circuit(0, 100000, RL=30000, Vin=10)
-
-    # start the simulation
-    asyncio.run(circuit.start())
+    # Start the simulation using asyncio.run()
+    asyncio.run(main())
